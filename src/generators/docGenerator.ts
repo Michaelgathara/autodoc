681427f17232ba17e0c400b6f5c21a9fc0cba06c
michaelgathara <mikegtrm@gmail.com>
2025-11-26 22:23:38 -0600

    chore removed useless import

--- src/generators/docGenerator.ts
+++ src/generators/docGenerator.ts
@@ -1,6 +1,5 @@
 import * as vscode from 'vscode';
 import { OpenRouterClient } from '../utils/openRouter';
-import { ConfigService } from '../config';
 
 export class DocGenerator {
     static async generate(document: vscode.TextDocument, symbol: vscode.DocumentSymbol): Promise<string | null> {
